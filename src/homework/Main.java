c73bac1735383b4bb3198bc3e1f5564b2a77c09b
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-08-29 13:46:04 -0400

    All Odd numbers from 1-100

---
+++ src/homework/Main.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+        //odd numbers from 1 to 100
+        for (int i=1; i<=100; i++){
+            if (i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
